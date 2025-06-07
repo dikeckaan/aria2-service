7a93b38021ab6c6c5844a705402c6ab19c413bfd
M. Galib Uludag <m231@tnctr.com>
2025-06-08 01:01:44 +0300

    Update aria2.ps1

--- aria2.ps1
+++ aria2.ps1
@@ -115,10 +115,6 @@ param (
     [string]$uri
 )
 
-$t = '[DllImport("user32.dll")] public static extern bool ShowWindow(int handle, int state);'
-add-type -name win -member $t -namespace native
-[native.win]::ShowWindow(([System.Diagnostics.Process]::GetCurrentProcess() | Get-Process).MainWindowHandle, 0)
-
 Add-Type -TypeDefinition @'
 using System;
 using System.Runtime.InteropServices;
